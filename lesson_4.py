3d1579e29e82256caf0cfa61a94d37c2c6c63b6d
Savinsky <savinsky.n@gmail.com>
2023-01-29 01:58:38 +0400

    ok

--- lesson_4.py
+++ lesson_4.py
@@ -1,7 +1,7 @@
 import random
 from collections import Counter
 
-n = 10
+n = 100
 lst = ['Nikita', 'Alex', 'Peter', 'Kate', 'Jonh', 'Fox', 'Mike', 'Max', 'Any', 'Marta',
        'Phill', 'Bob', 'Jim', 'Donald', 'Kim', 'Sasha', 'Andrew', 'Garry', 'Joe', 'Moon']
 def f(lst, n):
@@ -10,19 +10,17 @@ def f(lst, n):
         l.append(random.choice(lst))
     return l
 def most_common(lst):
-    max = 0
-    result = None
-    for x in set(lst):
-        count = lst.count(x)
-        if count > max:
-            max = count
-            result = x
-    return result
-    #data = Counter(lst)
-    #return data.most_common(1)[0][0]
+    data = Counter(lst)
+    return data.most_common(1)
+
+def rar_letter(lst):
+    d = Counter(lst)
+    v = d.most_common()[:-1-1:-1]
+    return v[0][0][0]
 
 l = f(lst, n)
 print(l)
 print(most_common(l))
+print(rar_letter(l))
 
 
